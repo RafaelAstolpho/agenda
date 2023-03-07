8214b4e5ffdbf5c654182b1c97f827342e2597e4
Fabricio <fabricio.palhavam@deciphex.com>
2023-03-07 14:17:13 -0300

    fixing pom

--- src/main/java/com/api/agenda/database/RepositorioContato.java
+++ src/main/java/com/api/agenda/database/RepositorioContato.java
@@ -3,7 +3,7 @@ package com.api.agenda.database;
 import com.api.agenda.entidade.Contato;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-@Repository
+
 public interface RepositorioContato extends JpaRepository <Contato, Long> {
 
 }
